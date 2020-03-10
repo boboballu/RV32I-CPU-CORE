8d7bbaca7b40d21b0f3d7fac81589cdf273ffd86
Tarun Govind <tkesava@ncsu.edu>
2020-03-10 13:04:15 -0400

    Perfectly working : added struct and instruction wise

--- my_5_stage_proc/packages.sv
+++ my_5_stage_proc/packages.sv
@@ -1,8 +1,8 @@
 package dbg_pkg;
     typedef struct {
         logic dmem_we;
-        logic dmem_addr; logic dmem_wd;
-        logic dmem_rd; 
+        logic [31:0] dmem_addr; logic [31:0] dmem_wd;
+        logic [31:0] dmem_rd; 
         logic regwriteM; logic memtoregM;
     }	mem_debug;
 endpackage
--- my_5_stage_proc/testbench.sv
+++ my_5_stage_proc/testbench.sv
@@ -35,14 +35,31 @@ module testbench();
 	// check results
 	always @(negedge clk) begin
 		if (memwrite) begin
-			$display ("pc: %d store : dataadr: %d  writedata: %d", pc, dataadr, $signed(writedata));
+			$display ("pc %d store : dataadr: %d writedata: %d", pc, dataadr, $signed(writedata));
 			if (dataadr===84 & $signed(writedata)===7) begin
 				$display("Simulation succeeded");
 				$stop;
 			end 
 		end
+		else if (debug.regwriteM) begin
+			if (debug.memtoregM) begin
+				$display ("pc %d load : dataadr: %d readdata: %d", pc, dataadr, $signed(readdata));
+			end
+			else begin
+				if (debug.dmem_addr === 32'bx) begin
+					$display ("pc %d nop/squashed instn-taken branch: data: %d", pc, dataadr);
+				end
+				else begin
+					$display ("pc %d ALU/Addi : data: %d", pc, dataadr);
+				end
+			end		
+		end
+
         else begin
-            $display ("pc %d load : dataadr: %d  readdata: %d", pc, dataadr, $signed(readdata));
+			if (dataadr === 0)
+            	$display ("pc %d bubble/branch : data: %d", pc, dataadr);
+			else 
+            	$display ("pc %d branch : data: %d", pc, dataadr);
         end
 	end
 endmodule
