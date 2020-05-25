f3166820d396318ced7418eaa268c3f5d3dee8b7
tkesava <tkesava@ncsu.edu>
2020-05-24 23:52:15 -0400

    Added --sc_dpi_example-- to the repo - for learning and reference

---
+++ my_5_stage_proc/dc/read.tcl
@@ -0,0 +1,22 @@
+#------------------------------------------------------------
+#
+# Basic Synthesis Script (TCL format)
+#                                  
+# Revision History                
+#   1/15/03  : Author Shane T. Gehring - from class example
+#   2/09/07  : Author Zhengtao Yu      - from class example
+#   12/14/07 : Author Ravi Jenkal      - updated to 180 nm & tcl
+#
+#------------------------------------------------------------
+
+#---------------------------------------------------------
+# Read in Verilog file and map (synthesize) onto a generic
+# library.
+# MAKE SURE THAT YOU CORRECT ALL WARNINGS THAT APPEAR
+# during the execution of the read command are fixed 
+# or understood to have no impact.
+# ALSO CHECK your latch/flip-flop list for unintended 
+# latches                                            
+#---------------------------------------------------------
+
+read_sverilog {$RTL_DIR/packages.sv $RTL_DIR/ff_stages.sv $RTL_DIR/comb_stages.sv $RTL_DIR/hazard_unit.sv $RTL_DIR/controller.sv $RTL_DIR/main.sv}
