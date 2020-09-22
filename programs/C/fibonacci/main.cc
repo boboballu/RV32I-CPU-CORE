d08b6fdd797d5a141a0b9aab1d994483d302a706
tkesava <tkesava@ncsu.edu>
2020-09-22 18:32:42 -0400

    test programs now in programs directory

--- programs/C/fibonacci/main.cc
+++ programs/C/fibonacci/main.cc
@@ -3,10 +3,10 @@ riscv64-unknown-elf-gcc -march=rv32i -mabi=ilp32 -O3 -nostdlib test1.c -o test1
 */
 int main()
 {
-    volatile int* tx = (volatile int*) 65532;
-    
+    volatile int* tx = (volatile int*) 65540;
+
 	int i, n = 12;
-	int t1 = 0, t2 = 1, nextTerm;	
+	int t1 = 0, t2 = 1, nextTerm;
 	for (i = 1; i <= n; ++i) {
         	*tx = t1;
         	nextTerm = t1 + t2;
