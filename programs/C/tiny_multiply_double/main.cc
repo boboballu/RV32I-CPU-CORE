17dfe9f28487167ea3e3cb7bc7b45fb622bc4624
Tarun <tkesava@ncsu.edu>
2020-10-15 23:26:10 -0400

    core fails for float - confirmed | emulator.cpp is a bug free emulator online, needs modification to run our binary | comparitive testbench is must

--- programs/C/tiny_multiply_double/main.cc
+++ programs/C/tiny_multiply_double/main.cc
@@ -4,14 +4,20 @@
 #include <stdarg.h>
 #include <assert.h>
 #include <math.h>
+// #include <stdio.h>
+
 
 int main () {
-    float x = 1.23;
-    float y = 2.98;
-    // 1.23 * 2.98 = 3.6654 
-    // double x[10] = {10.32, 17.67, 119.3216, -32.10, 37265.4467, -45672.85911, 1.21, 0, -1, 23};
-    // double y[10] = {86.471, 93.7312, -4.7236, -336.198, -665.334, 123, 4.67, 327.68, 4, 5};
-    _printf("float : %08x\n", (float)x*y);
-    //_printf("The product of %08x and %08x is : %08x", x, y, x*y);
+    float x = 38.6712;
+    float y = 22.7687;
+    float z = x*y;
+    //float_to_int_and_fract(z, integer);
+    int in, fr;
+    in = (int) z;
+    float fraction=0;
+    fraction = z - in;
+    fr = (int) (fraction * 10000);
+    _printf("fraction part : %d", fr);
+    //_printf("The product of %d and %d is : %d", (int)x, (int)y, (int) (x*y));
     return 0;
 }
